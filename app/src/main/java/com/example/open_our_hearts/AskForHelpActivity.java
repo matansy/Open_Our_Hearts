02ca59c279b1ff414caacd4903b3724399e96d57
matansy <matan.yishai@mail.huji.ac.il>
2021-05-22 14:00:20 +0300

    add ask for help

---
+++ app/src/main/java/com/example/open_our_hearts/AskForHelpActivity.java
@@ -0,0 +1,14 @@
+package com.example.open_our_hearts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AskForHelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_for_help);
+    }
+}
--- app/src/main/java/com/example/open_our_hearts/MainActivity.java
+++ app/src/main/java/com/example/open_our_hearts/MainActivity.java
@@ -10,14 +10,22 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button aboutUsBtn;
+    Button askForHelpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         aboutUsBtn = findViewById(R.id.whoWeAreBtn);
+        askForHelpBtn = findViewById(R.id.askForHelpBtn);
         aboutUsBtn.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), AboutUsActivity.class)));
+        askForHelpBtn.setOnClickListener(v ->{Intent intent = new Intent(MainActivity.this, siteContentShow.class);
+            intent.putExtra("siteURL", "https://docs.google.com/forms/d/1Ccc4kNLcCZTKQm3KO59pYPkJDvivz0gTVqcqMF9jXko/viewform?ts=5fa1aac3&edit_requested=true");
+            //intent.putExtra("scroll", 1558);
+            startActivity(intent);});
+
+
     }
 
 
