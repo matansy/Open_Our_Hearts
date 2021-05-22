9939381d22cdcb65c2de19dedd2c3ea589afece2
orrej <orrejwan>
2021-05-22 14:24:57 +0300

    Or

--- app/src/main/java/com/example/open_our_hearts/contact_page.java
+++ app/src/main/java/com/example/open_our_hearts/contact_page.java
@@ -2,7 +2,12 @@ package com.example.open_our_hearts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class contact_page extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class contact_page extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contact_page);
+		ImageButton btn_call_us = (ImageButton) findViewById(R.id.call_button);
+		String number = "0533444151";
+		btn_call_us.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_CALL);
+				intent.setData(Uri.parse("tel:" +number));
+				startActivity(intent);
+
+			}
+		});
 	}
 }
